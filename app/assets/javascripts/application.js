58b143fc471d3d6cf0c127679f6b118b96a1e3ac
Vicente <vincent_97@live.com.mx>
2016-01-13 14:11:20 -0600

    evitar errores al hacer deploy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //= require turbolinks
-//= require_tree ./dev
+// require_tree ./dev
 //= require_self
